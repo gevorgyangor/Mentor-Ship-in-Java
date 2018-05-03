c15d658bbd8df43e170541c39d40b742bbc3e980
Mic-User-05 <gevorgyan604@gmail.com>
2018-05-03 16:13:31 +0400

    change task

--- Spiral/src/Spiral.java
+++ Spiral/src/Spiral.java
@@ -1,32 +1,38 @@
 public class Spiral {
 
     public static void main(String[] args) {
-        int[][] spiral = createSpiral(5);
-//        System.out.println("-------------------------------");
-//        printSpiral(spiral);
+        int a = 27;
+        int b = (int) Math.sqrt(a);
+        int d = b * b;
+        if (a == d) {
+            int[][] spiral = createSpiral(b, a);
+        } else {
+            int[][] spiral = createSpiral(b + 1, a);
+        }
+
 
     }
 
-    private static int[][] createSpiral(int size) {
+    private static int[][] createSpiral(int size, int count) {
         int[][] spiral = new int[size][size];
-        int content = size * size;
+        int content = count;
         int start = 0;
         int startSpiral = 0;
         int endSpiral = size - 1;
 
-        while (start <= content) {
+        while (0 <= content) {
 
             for (int i = startSpiral; i <= endSpiral; i++) {
-                spiral[startSpiral][i] = start++;
+                spiral[startSpiral][i] = content--;
             }
             for (int j = startSpiral; j <= endSpiral; j++) {
-                spiral[j][endSpiral] = start++;
+                spiral[j][endSpiral] = content--;
             }
             for (int i = endSpiral - 1; i >= startSpiral; i--) {
-                spiral[endSpiral][i] = start++;
+                spiral[endSpiral][i] = content--;
             }
             for (int j = endSpiral - 1; j >= startSpiral + 1; j--) {
-                spiral[j][startSpiral] = start++;
+                spiral[j][startSpiral] = content--;
             }
 
             startSpiral++;
