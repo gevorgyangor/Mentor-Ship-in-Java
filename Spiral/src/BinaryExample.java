29336aadd8b2a6933df37d5a609f698c8976bc6a
Gor <gevorgyan604@gmail.com>
2018-05-17 00:25:23 +0400

    change task

--- Spiral/src/BinaryExample.java
+++ Spiral/src/BinaryExample.java
@@ -1,6 +1,13 @@
 public class BinaryExample {
     public static void main(String[] args) {
         binary(-3);
+        byte b = -100;
+        String[] str = byteToBits(b);
+        for (int i = 0; i < str.length; i++) {
+            String bs = str[i];
+            System.out.print(bs);
+        }
+        System.out.println();
     }
 
     private static void binary(int n) {
@@ -23,5 +30,27 @@ public class BinaryExample {
             n = n / 2;
         }
         return binaryNumber;
+    }
+
+    private static String[] byteToBits(byte b) {
+        String[] str = new String[8];
+        if (b <= 0) {
+            b = (byte) ((byte) ~b + 1);
+            str = byteToBiteOldNumber(b);
+            return str;
+        } else {
+            for (int i = 0; i < str.length; i++) {
+                str[(str.length - 1) - i] = "" + (b >> i) % 2;
+            }
+        }
+        return str;
+    }
+
+    private static String[] byteToBiteOldNumber(byte old) {
+        String[] str = new String[8];
+        for (int i = 0; i < str.length; i++) {
+            str[(str.length - 1) - i] = "" + (old >> i) % 2;
+        }
+        return str;
     }
 }
