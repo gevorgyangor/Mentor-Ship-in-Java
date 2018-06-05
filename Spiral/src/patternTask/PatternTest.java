6e0d094e666dbf6bad0fa715d0d73d32cf66296e
Mic-User-05 <gevorgyan604@gmail.com>
2018-06-05 14:26:39 +0400

    add Factory Pattern

--- Spiral/src/patternTask/PatternTest.java
+++ Spiral/src/patternTask/PatternTest.java
@@ -1,11 +1,13 @@
 package patternTask;
 
+import patternTask.model.User;
+
 public class PatternTest {
+
     public static void main(String[] args) {
+        User user = User.InstanceFactory.getInstance();
+        User user1 = User.InstanceFactory.getInstance();
+        User user2 = User.InstanceFactory.getInstance();
 
-    }
-    private static boolean addUserTest(){
-
-        return true;
     }
 }
