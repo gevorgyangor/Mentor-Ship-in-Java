6ccb6bea351c8fceb79a3de1a124f3f88f0a6977
Gor <gevorgyan604@gmail.com>
2018-06-21 21:59:03 +0400

    add task test project

--- testProject/src/main/TCPClient.java
+++ testProject/src/main/TCPClient.java
@@ -1,11 +1,14 @@
 package main;
 
+import main.db.DBConnectionProvider;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 class TCPClient {
+
     public static void main(String argv[]) throws Exception {
         String sentence;
         String modifiedSentence;
@@ -17,6 +20,6 @@ class TCPClient {
         outToServer.writeBytes(sentence + '\n');
         modifiedSentence = inFromServer.readLine();
         System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close();
+
     }
 }
--- testProject/src/main/TCPServer.java
+++ testProject/src/main/TCPServer.java
@@ -1,12 +1,21 @@
 package main;
 
+import main.db.DBConnectionProvider;
+import main.manager.EvenManager;
+import main.manager.OddManager;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 class TCPServer {
+    private static EvenManager evenManager = new EvenManager();
+    private static OddManager oddManager = new OddManager();
+
     public static void main(String argv[]) throws Exception {
         String clientSentence;
         String capitalizedSentence;
@@ -18,6 +27,16 @@ class TCPServer {
                     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             clientSentence = inFromClient.readLine();
+            String[] split = clientSentence.split(",");
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+            for (int i = 0; i < split.length; i++) {
+                if (Integer.parseInt(split[i]) % 2 == 0) {
+                    evenManager.addEvenNumber(Integer.parseInt(split[i]));
+                } else {
+                    oddManager.addOddNumber(Integer.parseInt(split[i]));
+                }
+            }
             System.out.println("Received: " + clientSentence);
             capitalizedSentence = clientSentence.toUpperCase() + '\n';
             outToClient.writeBytes(capitalizedSentence);
